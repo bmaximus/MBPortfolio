151900975de4f917651e0f7a265a4c1c04f487a0
Maxim Batiridis <batmax91@gmail.com>
2021-10-09 15:30:17 +0300

    Init commit

---
+++ MBPortfolio/Data/Experience.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBPortfolio.Data
+{
+    public class Experience
+    {
+        public Experience(DateTime dateFrom, DateTime? dateTo, string jobTitle, string companyName, List<string> duties)
+        {
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+            JobTitle = jobTitle;
+            CompanyName = companyName;
+            Duties = duties;
+        }
+
+        public DateTime DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+        public string JobTitle { get; set; }
+        public string CompanyName { get; set; }
+        public List<string> Duties { get; set; }
+    }
+}
---
+++ MBPortfolio/Data/ExperienceService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBPortfolio.Data
+{
+    public class ExperienceService
+    {
+        public Task<List<MBPortfolio.Data.Experience>> GetExperiences()
+        {
+
+            return Task.FromResult(new List<Experience>
+            {
+                new Experience(
+                new DateTime(2014, 3, 1),
+                new DateTime(2016, 4, 1),
+                jobTitle: "Junior Software Developer C#/SQL",
+                companyName: "Mouzenidis Group - Thessaloniki, Greece",
+                duties: new List<string>()
+                {
+                    "- Working with users to establish requirements and determine priorities",
+                    "- Enchancing features for existing systems on Web and Windows applications",
+                    "- Developing new application components to improve existing proccesses"
+                }
+                ),
+
+                new Experience(
+                new DateTime(2016, 5, 1),
+                new DateTime(2016, 10, 1),
+                jobTitle: "Software Developer C#/SQL",
+                companyName: "The Core Process - Nicosia, Cyprus",
+                duties: new List<string>()
+                {
+                    "- Complete development of e-shop on MVC",
+                    "- Complete development of ePOS for windows terminal machines"
+                }
+                ),
+
+                new Experience(
+                new DateTime(2016, 11, 1),
+                new DateTime(2017, 4, 1),
+                jobTitle: "Software Developer C#/SQL/Java/\"C/AL\" ",
+                companyName: "NetU Consultants - Nicosia, Cyprus",
+                duties: new List<string>()
+                {
+                    "- Developing new application components for existing public components",
+                    "- Working on web application with .NET and Angular",
+                    "- Participating on automotive industry integration development"
+                }
+                ),
+                new Experience(
+                new DateTime(2017, 4, 1),
+                null,
+                jobTitle: "Software Developer C#/SQL",
+                companyName: "Odyssey Consultants - Nicosia, Cyprus",
+                duties: new List<string>()
+                {
+                    "- Developing new application components for existing public components",
+                    "- Working on web application with .NET and Angular",
+                    "- Participating on automotive industry integration development"
+                }
+                )
+            }.OrderByDescending(d => d.DateFrom).ToList());
+        }
+
+    }
+}
